34114b990908a9c35c2ee3db5eb1274af154c4a2
markus_merten <markus.merten@eucon.com>
2024-12-07 12:52:12 +0100

    Einen test gefixt

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,17 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { AuthService } from '@services/auth.service';
+import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    authService = jasmine.createSpyObj('AuthService', ['logout']);
+    router = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        HeaderComponent, // Standalone Component muss hier importiert werden
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+      ],
+      providers: [
+        provideMockStore(),
+        { provide: AuthService, useValue: authService },
+        { provide: Router, useValue: router }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +39,14 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should render the toolbar with correct elements', () => {
+    const toolbar = fixture.debugElement.query(By.css('mat-toolbar'));
+    expect(toolbar).toBeTruthy();
+
+    const icons = fixture.debugElement.queryAll(By.css('mat-icon'));
+    expect(icons.length).toBeGreaterThan(0);
   });
 });
